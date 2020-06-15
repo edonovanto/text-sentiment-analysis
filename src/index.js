5c0c118078e2c584fccfe109bb2ae5676be2d813
Edo Novanto <novantoedo@gmail.com>
2020-06-15 21:45:49 +0700

    add file-loaders

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import './style/style.css'
 import main from './script/main.js'
 import './component/app-bar.js'
 import './component/search-bar.js'
+import img from '../assets/logo.png'
 
+document.getElementById('myLogo').src=img;
 document.addEventListener("DOMContentLoaded", main);
+
